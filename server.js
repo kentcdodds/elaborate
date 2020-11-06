e0caf320b5cd59fd8ee604837c30896151cc02af
Kent C. Dodds <me+github@kentcdodds.com>
2020-11-06 09:34:27 -0700

    git clone https://github.com/remix-run/starter-express.git

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const { createRequestHandler } = require("@remix-run/express");
+
+let app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.static("public"));
+
+app.get(
+  "*",
+  createRequestHandler({
+    getLoadContext() {
+      // Whatever you return here will be passed as `context` to your loaders.
+    }
+  })
+);
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Express server started on http://localhost:${port}`);
+});
